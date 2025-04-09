d6ffc46c4fa0e0d7d808e0e43e6a7f41af5f87dc
cocoth <anonymousid0000000@gmail.com>
2025-04-09 22:27:37 +0700

    fix bug update

--- src/app/api/surveys/[id]/route.ts
+++ src/app/api/surveys/[id]/route.ts
@@ -5,6 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
     const cookie = req.headers.get("cookie");
     const { id } = await params;
+    const body = await req.json()
 
     if (id) {
         const beRes = await fetch(`${process.env.BE_API_URL}/surveys/${id}`, {
@@ -14,7 +15,8 @@ export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id
                 ...(cookie && { Cookie: cookie }),
             },
             credentials: "include",
-            body: req.body,
+            body: JSON.stringify(body),
+
         });
         const data:ResponseAPI = await beRes.json();
         const res = NextResponse.json(data);
